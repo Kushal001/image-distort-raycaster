b0de8197900db9369d3d3664726d44b0835d82ee
Kushal001 <kushal121320@gmail.com>
2022-07-08 09:48:16 +0545

    raycaster implemented; plane follows mouse

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ const scene = new THREE.Scene()
 /**
  * Geometry
  */
-const geometry = new THREE.PlaneBufferGeometry(1, 1, 1)
+const geometry = new THREE.PlaneBufferGeometry(5, 5, 5)
 
 /**
  * Materials
@@ -27,6 +27,11 @@ const materials = new THREE.MeshBasicMaterial({})
 
 const mesh = new THREE.Mesh(geometry, materials)
 scene.add(mesh)
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
 
 /**
  * Sizes
@@ -51,16 +56,36 @@ window.addEventListener("resize", () => {
 })
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / sizes.width) * 2 - 1
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1
+})
+
+/**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   sizes.width / sizes.height,
+//   0.1,
+//   100
+// )
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera(
+  -4 * aspectRatio,
+  4 * aspectRatio,
+  4,
+  -4,
   0.1,
   100
 )
-camera.position.z = 1
+
+camera.position.z = 5
 scene.add(camera)
 
 // Controls
@@ -77,12 +102,37 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Red dot
+ */
+const dotGeometry = new THREE.PlaneBufferGeometry(1, 1, 1)
+
+/**
+ * Materials
+ */
+const dotMaterials = new THREE.MeshBasicMaterial({
+  color: "#ff0",
+})
+
+const dotMesh = new THREE.Mesh(dotGeometry, dotMaterials)
+scene.add(dotMesh)
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
+
+  // Cast ray
+  raycaster.setFromCamera(mouse, camera)
+
+  const objectsToTest = [mesh]
+  const intersects = raycaster.intersectObjects(objectsToTest)
+
+  if (intersects.length > 0) {
+    dotMesh.position.set(intersects[0].point.x, intersects[0].point.y, 2)
+  }
 
   // Update controls
   controls.update()
